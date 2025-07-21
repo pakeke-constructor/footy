d8e85c8beda1fd1833a2603dbdd5fe7069f1b09a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-07-21 18:49:21 +0700

    Add simple landmine

---
+++ scenes/objects/landmine/landmine.gd
@@ -0,0 +1,27 @@
+class_name Landmine
+extends SyncedRigidBody3D
+
+
+@export var explosion_force: float = 10.0
+
+@onready var detector: Area3D = $Detector
+
+
+func _ready() -> void:
+	super()
+	detector.body_entered.connect(_on_body_entered)
+
+
+func _physics_process(delta):
+	super._physics_process(delta)
+
+
+func _on_body_entered(body: Node) -> void:
+	if !multiplayer.is_server():
+		return
+	
+	if body is SyncedRigidBody3D:
+		var force_direction = (body.global_position - global_position).normalized()
+		body.apply_impulse(force_direction * explosion_force, body.global_position - global_position)
+		NetworkManager.debug("Boom!")
+		queue_free()
