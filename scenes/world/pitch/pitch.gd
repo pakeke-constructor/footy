4df9630a8d8c35a7f5f14fab7ab6641c70f7b9ea
Oli <OliverWDGarrett@gmail.com>
2025-07-22 14:55:41 +1200

    shell tex changes

--- scenes/world/pitch/pitch.gd
+++ scenes/world/pitch/pitch.gd
@@ -4,8 +4,13 @@ extends StaticBody3D
 @export var PITCH_LENGTH := 60;
 @export var PITCH_WIDTH := 40;
 
-const PLANE_SEPARATION := 0.2;
-const NUM_PLANES := 6;
+
+# Using shell-texturing
+const SHELL_SEPARATION := 0.2;
+const NUM_SHELLS := 12;
+
+
+const GRASS_PER_UNIT = 8;
 
 
 
@@ -16,14 +21,14 @@ func _ready() -> void:
 
 
 func create_shell_texture_layers():
-	var sep := PLANE_SEPARATION / NUM_PLANES
+	var sep := SHELL_SEPARATION / NUM_SHELLS
 	var shader = preload("res://scenes/world/pitch/shell_texture.gdshader")
 
-	for i in range(NUM_PLANES):
+	for i in range(NUM_SHELLS):
 		var mesh_instance := MeshInstance3D.new()
 		var mesh := PlaneMesh.new()
-		mesh.subdivide_width = PITCH_LENGTH
-		mesh.subdivide_depth = PITCH_WIDTH
+		# mesh.subdivide_width = PITCH_LENGTH
+		# mesh.subdivide_depth = PITCH_WIDTH
 		var size := Vector2(PITCH_LENGTH, PITCH_WIDTH)
 		mesh.size = size
 		mesh_instance.mesh = mesh
@@ -31,7 +36,7 @@ func create_shell_texture_layers():
 		var material := ShaderMaterial.new()
 		material.shader = shader
 		material.set_shader_parameter("shell_index", i)
-		material.set_shader_parameter("pitch_size", size)
+		material.set_shader_parameter("grass_grid_size", size * GRASS_PER_UNIT)
 		mesh_instance.material_override = material
 
 		var t = Transform3D()
