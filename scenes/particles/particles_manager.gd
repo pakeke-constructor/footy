a3ed74f37e737206d0ac39cd5bfed09d504a0dab
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-08-19 20:16:33 +0700

    Try to dedupe codes

--- scenes/particles/particles_manager.gd
+++ scenes/particles/particles_manager.gd
@@ -3,13 +3,14 @@ extends Node
 
 func kick(position: Vector3) -> void:
 	var scene := preload("res://scenes/particles/kick/kick.tscn")
-	var instance: Node3D = scene.instantiate()
-	instance.transform.origin = position
-	add_child(instance)
+	_spawn_particles(scene.instantiate(), position)
 
 
 func explosion(position: Vector3) -> void:
 	var scene := preload("res://scenes/particles/explosion/explosion.tscn")
-	var instance: Node3D = scene.instantiate()
+	_spawn_particles(scene.instantiate(), position)
+
+
+func _spawn_particles(instance: Node3D, position: Vector3) -> void:
 	instance.transform.origin = position
 	add_child(instance)
